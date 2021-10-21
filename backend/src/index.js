e896df5852d633282ebe268fc4fb771f0eb7eb9b
Rahul <rahulchampion48@gmail.com>
2021-10-21 13:48:02 +0530

    backend complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,6 +32,62 @@ app.post("/mint", upload.single("image"), async (req, res) => {
   const multerReq = req;
   if (!multerReq.file) {
     res.status(500).json({ status: false, msg: "no file provided !!" });
+  } else {
+    const fileName = multerReq.file.filename;
+    await pinata
+      .testAuthentication()
+      .catch((err) => res.status(500).json(JSON.stringify(err)));
+    const readableStreamForFile = fs.createReadStream(`./upoads/${fileName}`);
+    const options = {
+      pinataMetadata: {
+        name: req.body.title.replace(/\s/g, "-"),
+        keyvalues: {
+          description: req.body.description,
+        },
+      },
+    };
+    const pinnedFile = await pinata.pinFileToIPFS(
+      readableStreamForFile,
+      options
+    );
+    if (pinnedFile.IpfsHash && pinnedFile.PinSize > 0) {
+      fs.unlinkSync(`./uploads/${fileName}`);
+      // pins metadata
+      const metadata = {
+        name: req.body.title,
+        description: req.body.description,
+        symbol: "TUT",
+        artifactUri: `ipfs://${pinnedFile.IpfsHash}`,
+        displayUri: `ipfs://${pinnedFile.IpfsHash}`,
+        creators: [req.body.creator],
+        decimals: 0,
+        thumbnailUri: "https://tezostaquito.io/img/favicon.png",
+        is_transferable: true,
+        shouldPreferSymbol: false,
+      };
+
+      const pinnedMetadata = await pinata.pinJSONToIPFS(metadata, {
+        pinataMetadata: {
+          name: "TUT-metadata",
+        },
+      });
+
+      if (pinnedMetadata.IpfsHash && pinnedMetadata.PinSize > 0) {
+        res.status(200).json({
+          status: true,
+          msg: {
+            imageHash: pinnedFile.IpfsHash,
+            metadataHash: pinnedMetadata.IpfsHash,
+          },
+        });
+      } else {
+        res
+          .status(500)
+          .json({ status: false, msg: "metadata were not pinned" });
+      }
+    } else {
+      res.status(500).json({ status: false, msg: "file was not pinned" });
+    }
   }
 });
 
