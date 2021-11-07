e9fc1125c192154c61335f4c052067214f41d55e
Sanket Nighot <60044186+sanketnighot@users.noreply.github.com>
2021-11-07 12:49:30 +0530

    Update NFT Contract.py

--- contracts/NFT Contract.py
+++ contracts/NFT Contract.py
@@ -1,65 +1,83 @@
 import smartpy as sp
 
 class FA2ErrorMessage:
-    PREFIX = "FA2_"
-    TOKEN_UNDEFINED = "{}TOKEN_UNDEFINED".format(PREFIX)
-    INSUFFICIENT_BALANCE = "{}INSUFFICIENT_BALANCE".format(PREFIX)
-    NOT_OWNER = "{}NOT_OWNER".format(PREFIX)
-    OPERATORS_UNSUPPORTED = "{}OPERATORS_UNSUPPORTED".format(PREFIX)
+    PREFIX                   = "FA2_"
+    TOKEN_UNDEFINED          = "{}TOKEN_UNDEFINED".format(PREFIX)
+    INSUFFICIENT_BALANCE     = "{}INSUFFICIENT_BALANCE".format(PREFIX)
+    NOT_OWNER                = "{}NOT_OWNER".format(PREFIX)
+    OPERATORS_UNSUPPORTED    = "{}OPERATORS_UNSUPPORTED".format(PREFIX)
 
 class FKBErrorMessage:
     PREFIX = "FKB_"
-    CREATION_LIMIT_EXCEEDED = "{}CREATION_LIMIT_EXCEEDED".format(PREFIX)
-    CANT_MINT_SAME_TOKEN_TWICE = "{}CANT_MINT_SAME_TOKEN_TWICE".format(PREFIX)
-    CONTRACT_IS_PAUSED = "{}CONTRACT_IS_PAUSED".format(PREFIX)
-    MIN_VALUE_SHOULD_BE_MORE_THAN_ZERO = "{}MIN_VALUE_SHOULD_BE_MORE_THAN_ZERO".format(
-        PREFIX)
-    INCORRECT_PURCHASE_VALUE = "{}INCORRECT_PURCHASE_VALUE".format(PREFIX)
+    CREATION_LIMIT_EXCEEDED              = "{}CREATION_LIMIT_EXCEEDED".format(PREFIX)
+    CANT_MINT_SAME_TOKEN_TWICE           = "{}CANT_MINT_SAME_TOKEN_TWICE".format(PREFIX)
+    CONTRACT_IS_NOT_ACTIVE                   = "{}CONTRACT_IS_NOT_ACTIVE".format(PREFIX)
+    MIN_VALUE_SHOULD_BE_MORE_THAN_ZERO   = "{}MIN_VALUE_SHOULD_BE_MORE_THAN_ZERO".format(PREFIX)
+    INCORRECT_PURCHASE_VALUE             = "{}INCORRECT_PURCHASE_VALUE".format(PREFIX)
 
-# Declaring Ledger Key-Value types
-class LedgerKey:
+# Declaring Object Ledger Key-Value types
+class ObjectLedgerValue:
     def get_type():
-        return sp.TRecord(owner=sp.TAddress, token_id=sp.TNat).layout(("owner", "token_id"))
-    def make(owner, token_id):
-        return sp.set_type_expr(sp.record(owner=owner, token_id=token_id), LedgerKey.get_type())
+        return sp.TRecord(
+            object_id            = sp.TNat,
+            object_name          = sp.TString,
+            object_type          = sp.TString,
+            object_location      = sp.TString,
+            object_owner         = sp.TAddress,
+            object_is_shareable  = sp.TBool).layout(("object_id", "object_name", "object_type", "object_location", "object_owner", "object_is_shareable"))
+    
+    def make(object_id, object_name, object_type, object_location, object_owner, object_is_shareable):
+        return sp.set_type_expr(sp.record(object_id= object_id, object_name= object_name, object_type= object_type, object_location= object_location, object_owner= object_owner, object_is_shareable= object_is_shareable), ObjectLedgerValue.get_type())
+
+# Declaring Character Ledger Key-Value types
+class CharacterLedgerValue:
+    def get_type():
+        return sp.TRecord(
+            character_id         = sp.TNat, 
+            character_name       = sp.TString,
+            character_dna        = sp.TRecord(
+                                    head     = sp.TNat,
+                                    jeans    = sp.TNat,
+                                    logo     = sp.TNat,
+                                    shirt    = sp.TNat,
+                                    shoes    = sp.TNat),
+            character_location   = sp.TString,
+            character_owner      = sp.TAddress).layout("character_id", "character_name", "character_dna", "character_location", "character_owner")
+    
+    def make(character_id, character_name, character_dna, character_location, character_owner):
+        return sp.set_type_expr(sp.record(character_id= character_id, character_name= character_name, character_dna= character_dna, character_location= character_location, character_owner= character_owner), CharacterLedgerValue.get_value())
 
 # Declaring Token metadata Key-Value types
 class TokenMetadataValue:
     def get_type():
         return sp.TRecord(
-            token_id=sp.TNat,
-            token_info=sp.TMap(sp.TString, sp.TBytes)
+            token_id    = sp.TNat,
+            token_info  = sp.TMap(sp.TString, sp.TBytes)
         ).layout(("token_id", "token_info"))
 
 # Declaring Marketplace Key-Value types
 class marketplace:
     def get_value_type():
         return sp.TRecord(
-            seller=sp.TAddress,
-            sale_value=sp.TMutez,
+            seller      = sp.TAddress,
+            sale_value  = sp.TMutez,
         )
     def get_key_type():
         """ FKB Token ID """
         return sp.TNat
 
-# Declaring Token Key-Value types
-class TokenValue:
-    def get_type():
-        return sp.TRecord(global_card_id=sp.TNat, player_id=sp.TNat, year=sp.TNat, type=sp.TString, edition_no=sp.TNat, ipfs_string=sp.TString).layout(("global_card_id", ("player_id", ("year", ("type", ("edition_no", ("ipfs_string")))))))
-
 # Smart Contract 
 class FKB(sp.Contract):
     def __init__(self, admin, metadata):
         self.init(
-            administrator = admin,
-            metadata = metadata,
-            all_tokens=sp.set(t=sp.TNat),
-            character_ledger=sp.big_map(tkey=LedgerKey.get_type(), tvalue=sp.TNat),
-            objects_ledger=sp.big_map(tkey=LedgerKey.get_type(), tvalue=sp.TNat),
-            token_metadata=sp.big_map(tkey=sp.TNat, tvalue=TokenMetadataValue.get_type()),
-            marketplace=sp.big_map(tkey=marketplace.get_key_type(), tvalue=marketplace.get_value_type()),
-            paused=False,
-            tokens=sp.big_map(tkey=sp.TNat, tvalue=TokenValue.get_type()),
+            administrator    = admin,
+            metadata         = metadata,
+            character_ledger = sp.big_map(tkey=ObjectLedgerValue.get_type(), tvalue=sp.TNat),
+            objects_ledger   = sp.big_map(tkey=CharacterLedgerValue.get_type(), tvalue=sp.TNat),
+            token_metadata   = sp.big_map(tkey=sp.TNat, tvalue=TokenMetadataValue.get_type()),
+            marketplace      = sp.big_map(tkey=marketplace.get_key_type(), tvalue=marketplace.get_value_type()),
+            active           = True,
+            tokens           = sp.big_map(tkey=sp.TNat, tvalue=sp.TString),
         )
 
     def is_administrator(self, sender):
@@ -71,18 +89,18 @@ class FKB(sp.Contract):
                   message=FA2ErrorMessage.NOT_OWNER)
         self.data.administrator = params
 
-    def is_paused(self):
-        return self.data.paused
+    def is_active(self):
+        return self.data.active
 
     @sp.entry_point
-    def set_pause(self, params):
+    def toggle_active(self):
         sp.verify(self.is_administrator(sp.sender),
                   message=FA2ErrorMessage.NOT_OWNER)
-        self.data.paused = params
+        self.data.active = ~self.data.active
 
     @sp.entry_point
     def mint(self, params):
-        sp.verify(~self.is_paused(), FKBErrorMessage.CONTRACT_IS_PAUSED)
+        sp.verify(self.is_active(), FKBErrorMessage.CONTRACT_IS_NOT_ACTIVE)
         sp.verify(self.is_administrator(sp.sender),
                   message=FA2ErrorMessage.NOT_OWNER)
         token_id = sp.len(self.data.all_tokens)
@@ -99,7 +117,7 @@ class FKB(sp.Contract):
 
     @sp.entry_point
     def transfer(self, batch_transfers):
-        sp.verify(~self.is_paused(), FKBErrorMessage.CONTRACT_IS_PAUSED)
+        sp.verify(self.is_active(), FKBErrorMessage.CONTRACT_IS_NOT_ACTIVE)
         sp.set_type(batch_transfers, BatchTransfer.get_type())
         sp.for transfer in batch_transfers:
             sp.for tx in transfer.txs:
