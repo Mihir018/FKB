11a5ffc3c2a3c797f27e47015e24c6145dcedce0
Sanket Nighot <60044186+sanketnighot@users.noreply.github.com>
2021-11-08 14:07:38 +0530

    Update NFT Contract.py

--- contracts/NFT Contract.py
+++ contracts/NFT Contract.py
@@ -1,52 +1,56 @@
 import smartpy as sp
 
 class FA2ErrorMessage:
-    PREFIX                   = "FA2_"
-    TOKEN_UNDEFINED          = "{}TOKEN_UNDEFINED".format(PREFIX)
-    INSUFFICIENT_BALANCE     = "{}INSUFFICIENT_BALANCE".format(PREFIX)
-    NOT_OWNER                = "{}NOT_OWNER".format(PREFIX)
-    OPERATORS_UNSUPPORTED    = "{}OPERATORS_UNSUPPORTED".format(PREFIX)
+    PREFIX                  = "FA2 : "
+    TOKEN_UNDEFINED         = "{}TOKEN_UNDEFINED".format(PREFIX)
+    INSUFFICIENT_BALANCE    = "{}INSUFFICIENT_BALANCE".format(PREFIX)
+    NOT_OWNER               = "{}NOT_OWNER".format(PREFIX)
+    OPERATORS_UNSUPPORTED   = "{}OPERATORS_UNSUPPORTED".format(PREFIX)
 
 class FKBErrorMessage:
-    PREFIX = "FKB_"
-    CANT_MINT_SAME_TOKEN_TWICE           = "{}CANT_MINT_SAME_TOKEN_TWICE".format(PREFIX)
-    CONTRACT_IS_NOT_ACTIVE               = "{}CONTRACT_IS_NOT_ACTIVE".format(PREFIX)
-    MIN_VALUE_SHOULD_BE_MORE_THAN_ZERO   = "{}MIN_VALUE_SHOULD_BE_MORE_THAN_ZERO".format(PREFIX)
-    INCORRECT_PURCHASE_VALUE             = "{}INCORRECT_PURCHASE_VALUE".format(PREFIX)
-    OBJECT_IS_NOT_SHAREABLE              = "{}OBJECT_IS_NOT_SHAREABLE".format(PREFIX)
+    PREFIX                              = "FKB : "
+    CANT_MINT_SAME_TOKEN_TWICE          = "{}CANT_MINT_SAME_TOKEN_TWICE".format(PREFIX)
+    CONTRACT_IS_NOT_ACTIVE              = "{}CONTRACT_IS_NOT_ACTIVE".format(PREFIX)
+    MIN_VALUE_SHOULD_BE_MORE_THAN_ZERO  = "{}MIN_VALUE_SHOULD_BE_MORE_THAN_ZERO".format(PREFIX)
+    INCORRECT_PURCHASE_VALUE            = "{}INCORRECT_PURCHASE_VALUE".format(PREFIX)
+    OBJECT_IS_NOT_SHAREABLE             = "{}OBJECT_IS_NOT_SHAREABLE".format(PREFIX)
 
 
 # Declaring Object Ledger Key-Value types
 class ObjectLedgerValue:
     def get_type():
         return sp.TRecord(
-            object_id            = sp.TNat,
-            object_name          = sp.TString,
-            object_type          = sp.TString,
-            object_location      = sp.TString,
-            object_owner         = sp.TAddress,
-            object_is_shareable  = sp.TBool).layout(("object_id", "object_name", "object_type", "object_location", "object_owner", "object_is_shareable"))
-    
-    def make(object_id, object_name, object_type, object_location, object_owner, object_is_shareable):
-        return sp.set_type_expr(sp.record(object_id= object_id, object_name= object_name, object_type= object_type, object_location= object_location, object_owner= object_owner, object_is_shareable= object_is_shareable), ObjectLedgerValue.get_type())
+            object_id           = sp.TNat,
+            object_name         = sp.TString,
+            object_type         = sp.TString,
+            object_location     = sp.TString,
+            object_owner        = sp.TAddress,
+            object_is_shareable = sp.TBool,
+            object_amount       = sp.TMutez)
+            
+    def make(object_id, object_name, object_type, object_location, object_owner, object_is_shareable, object_amount):
+        return sp.set_type_expr(sp.record(object_id = object_id, object_name = object_name, object_type = object_type, object_location = object_location, object_owner = object_owner, object_is_shareable = object_is_shareable, object_amount = object_amount), ObjectLedgerValue.get_type())
 
 # Declaring Character Ledger Key-Value types
 class CharacterLedgerValue:
     def get_type():
         return sp.TRecord(
-            character_id         = sp.TNat, 
-            character_name       = sp.TString,
-            character_dna        = sp.TRecord(
-                                    head     = sp.TNat,
-                                    jeans    = sp.TNat,
-                                    logo     = sp.TNat,
-                                    shirt    = sp.TNat,
-                                    shoes    = sp.TNat),
-            character_location   = sp.TString,
-            character_owner      = sp.TAddress).layout("character_id", "character_name", "character_dna", "character_location", "character_owner")
-    
-    def make(character_id, character_name, character_dna, character_location, character_owner):
-        return sp.set_type_expr(sp.record(character_id= character_id, character_name= character_name, character_dna= character_dna, character_location= character_location, character_owner= character_owner), CharacterLedgerValue.get_value())
+            character_id        = sp.TNat, 
+            character_name      = sp.TString,
+            character_dna       = sp.TRecord(
+                    head    = sp.TNat,
+                    jeans   = sp.TNat,
+                    logo    = sp.TNat,
+                    shirt   = sp.TNat,
+                    shoes   = sp.TNat
+                    ),
+            character_location  = sp.TString,
+            character_owner     = sp.TAddress,
+            character_amount    = sp.TMutez
+            )
+            
+    def make(character_id, character_name, character_dna, character_location, character_owner, character_amount):
+        return sp.set_type_expr(sp.record(character_id = character_id, character_name = character_name, character_dna = character_dna, character_location = character_location, character_owner = character_owner, character_amount = character_amount), CharacterLedgerValue.get_type())
 
 # Declaring Token metadata Key-Value types
 class TokenMetadataValue:
@@ -105,9 +109,9 @@ class FKB(sp.Contract):
         token_id = sp.len(self.data.all_tokens)
         sp.verify(~ self.data.all_tokens.contains(token_id),
                     message = FKBErrorMessage.CANT_MINT_SAME_TOKEN_TWICE)
-        new_obj = ObjectLedgerValue.make(token_id, params.object_name, params.object_type, params.object_location, sp.sender, params.object_is_shareable)
+        new_obj = ObjectLedgerValue.make(token_id, params.object_name, params.object_type, params.object_location, sp.sender, params.object_is_shareable, params.object_amount)
         self.data.objects_ledger[token_id] = new_obj
-        token_metadata_value = sp.TRecord(
+        token_metadata_value = sp.record(
                                     token_id    = token_id,
                                     name        = params.object_name,
                                     ipfs_link   = params.object_location,
@@ -120,9 +124,9 @@ class FKB(sp.Contract):
         token_id = sp.len(self.data.all_tokens)
         sp.verify(~ self.data.all_tokens.contains(token_id),
                     message = FKBErrorMessage.CANT_MINT_SAME_TOKEN_TWICE)
-        new_char = CharacterLedgerValue.make(character_id, character_name, character_dna, character_location, character_owner)
-        self.data.objects_ledger[token_id] = new_char
-        token_metadata_value = sp.TRecord(
+        new_char = CharacterLedgerValue.make(character_id = token_id, character_name = params.character_name, character_dna = params.character_dna, character_location = params.character_location, character_owner = sp.sender, character_amount = params.character_amount)
+        self.data.characters_ledger[token_id] = new_char
+        token_metadata_value = sp.record(
                                     token_id    = token_id,
                                     name        = params.character_name,
                                     ipfs_link   = params.character_location,
@@ -132,7 +136,7 @@ class FKB(sp.Contract):
     @sp.entry_point
     def transfer_object(self, params):
         sp.verify(self.is_active(), message = FKBErrorMessage.CONTRACT_IS_NOT_ACTIVE)
-        sp.verify(self.data.objects_ledger[token_id].object_is_shareable, message = FKBErrorMessage.OBJECT_IS_NOT_SHAREABLE)
+        sp.verify(self.data.objects_ledger[params.token_id].object_is_shareable, message = FKBErrorMessage.OBJECT_IS_NOT_SHAREABLE)
         sp.verify_equal(sp.sender, self.data.objects_ledger[params.token_id].object_owner, message = FA2ErrorMessage.NOT_OWNER)
         self.data.objects_ledger[params.token_id].object_owner = params.new_owner
 
@@ -157,5 +161,139 @@ class FKB(sp.Contract):
     def buy_nft(self, params):
         sp.verify_equal(sp.amount, self.data.marketplace[params.token_id].sale_value, message = FKBErrorMessage.INCORRECT_PURCHASE_VALUE)
         self.data.characters_ledger[params.token_id].character_owner = params.new_owner
+        self.data.characters_ledger[params.token_id].character_amount = sp.amount
         del self.data.marketplace[params.token_id]
+
+if "templates" not in __name__:
+    @sp.add_test(name="FKB NFTs")
+    def test():
+        sc = sp.test_scenario()
+        sc.h1("FKB NFT's and Marketplace")
+        sc.h2("Accounts")
+        admin = sp.address("tz1LXRS2zgh12gbGix6R9xSLJwfwqM9VdpPW")
+        alice = sp.test_account("Alice")
+        bob = sp.test_account("Bob")
+        dan = sp.test_account("Dan")
+        sc.h1("NFT Contract")
+        fkb = FKB(admin = admin, metadata = "metadata")
+        sc += fkb
+        sc.h1("Table of Contents")
+        sc.table_of_contents()
+        sc.h1("Accounts")
+        sc.show([alice, bob, dan])
+        sc.h1("FKB NFT Contract")
+        sc.h2("Minting Objects")
+        obj1 = sp.record(
+                object_name          = "Head1",
+                object_type          = "head",
+                object_location      = "link.com",
+                object_is_shareable  = True,
+                object_amount        = sp.mutez(823)
+            )
+        obj2 = sp.record(
+                object_name          = "Jeans1",
+                object_type          = "jeans",
+                object_location      = "link.com",
+                object_is_shareable  = True,
+                object_amount        = sp.mutez(864)
+            )
+        obj3 = sp.record(
+                object_name          = "Logo1",
+                object_type          = "logo",
+                object_location      = "link.com",
+                object_is_shareable  = True,
+                object_amount        = sp.mutez(654)
+            )
+        obj4 = sp.record(
+                object_name          = "Shirt1",
+                object_type          = "shirt",
+                object_location      = "link.com",
+                object_is_shareable  = True,
+                object_amount        = sp.mutez(687)
+            )
+        obj5 = sp.record(
+                object_name          = "Shoes1",
+                object_type          = "shoes",
+                object_location      = "link.com",
+                object_is_shareable  = True,
+                object_amount        = sp.mutez(867)
+            )
+        sc += fkb.mint_object(obj1).run(sender = admin, amount = obj1.object_amount)
+        sc += fkb.mint_object(obj2).run(sender = admin, amount = obj2.object_amount)
+        sc += fkb.mint_object(obj3).run(sender = admin, amount = obj3.object_amount)
+        sc += fkb.mint_object(obj4).run(sender = admin, amount = obj4.object_amount)
+        sc += fkb.mint_object(obj5).run(sender = admin, amount = obj5.object_amount)
+        
+        sc.h2("Minting Characters")
+        char1 = sp.record(
+                character_name      = "Char 1",
+                character_dna       = sp.record(
+                                head    = 0,
+                                jeans   = 1,
+                                logo    = 2,
+                                shirt   = 3,
+                                shoes   = 4
+                            ),
+                character_location  = "char1.locn",
+                character_amount    = sp.mutez(3456)
+            )
+
+        char2 = sp.record(
+                character_name      = "Char 2",
+                character_dna       = sp.record(
+                                head    = 80,
+                                jeans   = 17,
+                                logo    = 62,
+                                shirt   = 39,
+                                shoes   = 564
+                            ),
+                character_location  = "char2.locn",
+                character_amount    = sp.mutez(8764)
+            )
+
+        char3 = sp.record(
+                character_name      = "Char 3",
+                character_dna       = sp.record(
+                                head    = 80,
+                                jeans   = 19,
+                                logo    = 872,
+                                shirt   = 63,
+                                shoes   = 894
+                            ),
+                character_location  = "char3.locn",
+                character_amount    = sp.mutez(3283)
+            )
+
+        char4 = sp.record(
+                character_name      = "Char 4",
+                character_dna       = sp.record(
+                                head    = 80,
+                                jeans   = 19,
+                                logo    = 8289,
+                                shirt   = 37,
+                                shoes   = 684
+                            ),
+                character_location  = "char4.locn",
+                character_amount    = sp.mutez(54895)
+            )
+
+        char5 = sp.record(
+                character_name      = "Char 5",
+                character_dna       = sp.record(
+                                head    = 5708,
+                                jeans   = 81,
+                                logo    = 27,
+                                shirt   = 8653,
+                                shoes   = 454
+                            ),
+                character_location  = "char5.locn",
+                character_amount    = sp.mutez(23244)
+            )
+
+        
+        sc += fkb.mint_character(char1).run(sender = admin, amount = char1.character_amount)
+        sc += fkb.mint_character(char2).run(sender = admin, amount = char2.character_amount)
+        sc += fkb.mint_character(char3).run(sender = admin, amount = char3.character_amount)
+        sc += fkb.mint_character(char4).run(sender = admin, amount = char4.character_amount)
+        sc += fkb.mint_character(char5).run(sender = admin, amount = char5.character_amount)
 
